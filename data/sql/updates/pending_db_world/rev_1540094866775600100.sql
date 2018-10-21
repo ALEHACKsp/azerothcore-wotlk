d53ce253afa3ce5b0c40df0ac30ae4975ee5c4af
Jinyang Li <jli134@hawk.iit.edu>
2018-10-21 12:10:55 +0800

    sql fix for custom

---
+++ data/sql/updates/pending_db_world/rev_1540094866775600100.sql
@@ -0,0 +1,5 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1540094866775600100');
+
+insert into acore_world.spell_custom_attr values 
+(    12579,                               4194304);
+-- winter'schill                SPELL_ATTR0_CU_SINGLE_AURA_STACK
