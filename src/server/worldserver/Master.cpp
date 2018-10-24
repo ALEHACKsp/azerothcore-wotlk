4cead0dc008aadd17bcd84f40c74eb09fba75c42
Jinyang Li <jli134@hawk.iit.edu>
2018-10-24 10:11:58 +0800

    display improve on start-up

--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
  * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
@@ -216,7 +216,7 @@ int Master::Run()
     if (highPriority)
     {
         if (SetPriorityClass(hProcess, HIGH_PRIORITY_CLASS))
-            sLog->outString("worldserver process priority class set to HIGH");
+            sLog->outString("worldserver process priority class set to HIGH\n");
         else
             sLog->outError("Can't set worldserver process priority class.");
     }
